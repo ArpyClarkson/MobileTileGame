b302ee9573fac6a30d467b6e10382be913cbd217
dan-wade <danwadeski@gmail.com>
2021-01-26 01:43:57 -0600

    uwu

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,10 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 
-public class Grid {
+public class GameState {
 	int[,] grid = new int[13,13];
 
-	public Grid() {
+	public GameState() {
 		for(int x = 0; x < 13; x++) {
 			for(int y = 0; y < 13; y++) {
 				grid[x, y] = 0;
