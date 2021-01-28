c6ba00d6f5c772e8353c7ba53c3ad4c6c10cba0e
dan-wade <danwadeski@gmail.com>
2021-01-27 23:38:01 -0600

    poggy c:

---
+++ Assets/Editor/AutoRefresher.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using UnityEngine;
+using UnityEditor;
+
+[InitializeOnLoad]
+public static class AutoRefresher {
+    static FileSystemWatcher watcher;
+	static bool shouldRefresh = false;
+
+	static AutoRefresher() {
+		watcher = new FileSystemWatcher(Application.dataPath);	
+		watcher.NotifyFilter = NotifyFilters.LastWrite| NotifyFilters.FileName;
+		watcher.IncludeSubdirectories = true;
+		watcher.EnableRaisingEvents = true;
+
+		EditorApplication.update += () => {
+			if(shouldRefresh) {
+				shouldRefresh = false;
+				AssetDatabase.Refresh();
+
+				foreach(var window in Resources.FindObjectsOfTypeAll<EditorWindow>())
+					window.Repaint();
+			}
+		};
+		
+		watcher.Changed += (s, e) => shouldRefresh = true;
+	}
+}
