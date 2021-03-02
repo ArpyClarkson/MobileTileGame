3e2a36d46a602b087e1cecb7bd219da88a953d65
dan-wade <danwadeski@gmail.com>
2021-03-02 00:04:13 -0600

    Update GridManager.cs

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -15,7 +15,7 @@ public class GridManager : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
-        tg = new TileGrid( GridSizeX, GridSizeY );
+       // tg = new TileGrid( GridSizeX, GridSizeY );
 
         
     }
