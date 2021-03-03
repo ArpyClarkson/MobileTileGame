e0a2e411f8fd6c1f35ed4f9e4da44e9e326ddc20
Andrew <arpyclarkson@gmail.com>
2021-03-02 23:28:08 -0600

    render shit update

--- Assets/Scripts/GridRenderer.cs
+++ Assets/Scripts/GridRenderer.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 
 public class GridRenderer : MonoBehaviour {
 
-    GameObject tile;
+    public GameObject tile;
 
     // Start is called before the first frame update
     void Start() {
