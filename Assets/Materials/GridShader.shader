b297418733e6d5d2e78c7a70b335d891ad781d2b
dan-wade <danwadeski@gmail.com>
2021-03-03 16:05:52 -0600

    hard mode c:

--- Assets/Materials/GridShader.shader
+++ Assets/Materials/GridShader.shader
@@ -26,26 +26,33 @@ Pass {
         return o;
     }
 
-    uint _grid[1024];
+    uint grid[1024];
+    float2 gridSize;
     
-
     fixed3 frag(v2f i) : SV_Target {
-        float2 GridSize = float2(9,13);
-        uint2 index = i.uv*GridSize;
-        uint value = _grid[index.x + GridSize.x*index.y];
+        i.uv.y = (i.uv.y+_Time.x*(floor(i.uv.x*gridSize.x) - gridSize.x/2.f))%1.0f;
+        uint2 index = abs(i.uv)*gridSize;
+        uint value = grid[index.x + gridSize.x*index.y];
+
+        
 
         float3 result = 0;
         if(value == 1) result = float3(1,0,0);
         if(value == 2) result = float3(0,1,0);
         if(value == 3) result = float3(0,0,1);
+        if(value == 4) result = float3(1,0,1);
 
-        float2 fr = frac(i.uv*GridSize);
+        float2 fr = frac(i.uv*gridSize);
         fr = float2(distance(fr.x, 0.5f), distance(fr.y, 0.5f));
         float m = 1.f-max(fr.x, fr.y);
-        if(m > 0.52f) return result*m*m*m;
-        return sin(_Time.w + i.uv.x + i.uv.y);
+        if(m > 0.58f+ sin((i.uv.x+i.uv.y)*10.0f+_Time.w)*0.08f) return result*m*m*m;
 
-        return float4(frac(i.uv*float2(9,13)), 0, 0);
+        i.uv *= 3.14f;
+        return float3(
+            sin(i.uv.x-i.uv.y+_Time.y) * ((i.uv.x + i.uv.y+_Time.x)%1.f),
+            cos(-i.uv.x+i.uv.y+_Time.y) * ((i.uv.x - i.uv.y+_Time.x)%1.f),
+            sin(i.uv.x+i.uv.y+_Time.y) * ((-i.uv.x - i.uv.y+_Time.x)%1.f)
+        );
     }
     ENDCG
 }
--- Assets/Scripts/WodeScripts/MobileTileGame.cs
+++ Assets/Scripts/WodeScripts/MobileTileGame.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class MobileTileGame : MonoBehaviour, IPointerDownHandler {
 	IGrid grid;
 	float[] buf;
 
-	void Start() {
+	void OnEnable() {
 		Application.targetFrameRate = 30;
 
         //grid = new GameState();
@@ -15,10 +16,21 @@ public class MobileTileGame : MonoBehaviour, IPointerDownHandler {
 		grid.Generate(9, 13);
 		buf = new float[grid.GridSizeX*grid.GridSizeY];
 
+		var gridImage = FindObjectOfType<RawImage>();
+		var trigger = gridImage.gameObject.AddComponent<EventTrigger>();
+		EventTrigger.Entry entry = new EventTrigger.Entry();
+		entry.eventID = EventTriggerType.PointerClick;
+		entry.callback.AddListener((data) => {
+			Debug.Log((data as PointerEventData ).position / gridImage.rectTransform.rect.size);
+		});
+		trigger.triggers.Add(entry);
+
 		//var gr = gameObject.GetComponent<GridRenderer>();
 	}
 
 	void Update() {
+		if(grid == null)
+			return;
 
 		for (int x = 0; x < grid.GridSizeX; x++) {
 			for (int y = 0; y < grid.GridSizeY; y++) {
@@ -26,15 +38,16 @@ public class MobileTileGame : MonoBehaviour, IPointerDownHandler {
 			}
 		}
 
-		Shader.SetGlobalFloatArray("_grid", buf);
+		Shader.SetGlobalVector("gridSize", new Vector4(grid.GridSizeX, grid.GridSizeY));
+		Shader.SetGlobalFloatArray("grid", buf);
+	}
 
-		if(Input.GetMouseButtonDown(0)) {
-			
-		}
+	void OnDisable() {
 
 	}
 
 	public void OnPointerDown(PointerEventData eventData) {
-		Debug.Log("yayay onpointerdown yayayay");
+
+		//Debug.Log(eventData.position);
 	}
 }
